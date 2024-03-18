28ef414e641499bcf4d636ba95a50cc121761b44
matchati <tehmatcha28@gmail.com>
2024-03-18 19:04:13 +0800

    chore : update post test 3

--- Posttest_APL_3/2309106065_AdiMuhammadSyifai_POSTTEST3.cpp
+++ Posttest_APL_3/2309106065_AdiMuhammadSyifai_POSTTEST3.cpp
@@ -2,37 +2,106 @@
 
 using namespace std;
 
-// Prosedur 1
-void clearScreen();
+// Fungsi Prosedur 1
+void clearScreen() { system("cls || clear"); };
 
-// Prosedur 2
-void enterContinue();
-
-// Prosedur 3
-void menu();
+// Fungsi Prosedur 2
+void enterContinue()
+{
+    cout << "\n Tekan enter untuk melanjutkan...";
+    cin.ignore();
+    cin.get();
+    clearScreen();
+}
 
 // Fungsi 1
-float luasLingkaran(float jariJari);
+bool login();
 
 // Fungsi 2
-float luasSegitiga(float alas, float tinggi);
+int menu(int pilihan);
 
 // Fungsi Rekursif
-int faktorial(int n);
+void showKeyboardsRecursively(string keyboards[], int index, int jumlahKeyboard)
+{
+    if (index == jumlahKeyboard)
+        return;
 
-// Fungsi Utama
+    cout << index + 1 << ". " << keyboards[index] << endl;
+    showKeyboardsRecursively(keyboards, index + 1, jumlahKeyboard);
+}
+
+// Funsgi Prosedur 3
+void addKeyboard(string keyboards[], int &jumlahKeyboard)
+{
+    string namaKeyboard;
+    cout << "Masukkan nama keyboard: ";
+    cin >> namaKeyboard;
+    keyboards[jumlahKeyboard] = namaKeyboard;
+    jumlahKeyboard++;
+    cout << "Keyboard berhasil ditambahkan" << endl;
+}
+
+// Fungsi Prosedur 4
+void editKeyboard(string keyboards[], int jumlahKeyboard)
+{
+    int index;
+    cout << "Pilih keyboard yang akan diubah: ";
+    cin >> index;
+    if (index >= 1 && index <= jumlahKeyboard)
+    {
+        cout << "Masukkan nama keyboard baru: ";
+        cin >> keyboards[index - 1];
+        cout << "Keyboard berhasil diubah" << endl;
+    }
+    else
+    {
+        cout << "Index tidak tersedia";
+    }
+}
+
+// Fungsi Prosedur 5
+void deleteKeyboard(string keyboards[], int &jumlahKeyboard)
+{
+    int index;
+    cout << "Pilih keyboard yang akan dihapus: ";
+    cin >> index;
+    if (index >= 1 && index <= jumlahKeyboard)
+    {
+        for (int i = index - 1; i < jumlahKeyboard; i++)
+        {
+            keyboards[i] = keyboards[i + 1];
+        }
+        jumlahKeyboard--;
+        cout << "Keyboard berhasil dihapus" << endl;
+    }
+    else
+    {
+        cout << "Index tidak tersedia";
+    }
+}
+
 int main()
 {
+    const int MAX_KEYBOARD = 10;
+    string keyboards[MAX_KEYBOARD] = {"Maxfit61", "Orbiter67"};
+    int jumlahKeyboard = 2;
     clearScreen();
+
+    bool isLogin = login();
+
+    if (!isLogin)
+    {
+        clearScreen();
+        cout << "Anda telah mencoba 3 kali, coba lagi nanti" << endl;
+        return 0;
+    }
+
     int pilihan;
 
     do
     {
         clearScreen();
-        menu();
-        float hasil;
-        cout << "Pilihan: ";
-        cin >> pilihan;
+        pilihan = menu(pilihan);
 
         switch (pilihan)
         {
@@ -41,34 +110,24 @@ int main()
             break;
         case 1:
             clearScreen();
-            cout << "Menghitung Luas Lingkaran" << endl;
-            float jariJari;
-            cout << "Masukkan jari-jari: ";
-            cin >> jariJari;
-            hasil = luasLingkaran(jariJari);
-            cout << "Luas lingkaran dengan jari-jari " << jariJari << " adalah " << hasil << endl;
+            showKeyboardsRecursively(keyboards, 0, jumlahKeyboard);
             enterContinue();
             break;
         case 2:
             clearScreen();
-            cout << "Menghitung Luas Segitiga" << endl;
-            float alas, tinggi;
-            cout << "Masukkan alas: ";
-            cin >> alas;
-            cout << "Masukkan tinggi: ";
-            cin >> tinggi;
-            hasil = luasSegitiga(alas, tinggi);
-            cout << "Luas segitiga dengan alas " << alas << " dan tinggi " << tinggi << " adalah " << hasil << endl;
+            addKeyboard(keyboards, jumlahKeyboard);
             enterContinue();
             break;
         case 3:
             clearScreen();
-            cout << "Menghitung Hasil Faktorial" << endl;
-            int n;
-            cout << "Masukkan n: ";
-            cin >> n;
-            hasil = faktorial(n);
-            cout << "Hasil faktorial dari " << n << " adalah " << hasil << endl;
+            showKeyboardsRecursively(keyboards, 0, jumlahKeyboard);
+            editKeyboard(keyboards, jumlahKeyboard);
+            enterContinue();
+            break;
+        case 4:
+            clearScreen();
+            showKeyboardsRecursively(keyboards, 0, jumlahKeyboard);
+            deleteKeyboard(keyboards, jumlahKeyboard);
             enterContinue();
             break;
         default:
@@ -80,51 +139,54 @@ int main()
     } while (pilihan != 0);
 
     clearScreen();
-    cout << "Terima kasih telah menggunakan aplikasi ini" << endl;
-    return 0;
+    cout << "Program selesai" << endl;
 }
 
-void clearScreen() { system("cls || clear"); };
-
-void enterContinue()
+bool login()
 {
-    cout << "\n Tekan enter untuk melanjutkan...";
-    cin.ignore();
-    cin.get();
-    clearScreen();
+
+    int counter = 0;
+    bool isLogin = false;
+
+    while (counter < 3 && !isLogin)
+    {
+        string nama, nim;
+
+        cout << "Masukkan Nama: ";
+        cin >> nama;
+        cout << "Masukkan NIM: ";
+        cin >> nim;
+
+        if (nama == "Adi" && nim == "2309106065")
+        {
+            isLogin = true;
+        }
+        else
+        {
+            clearScreen();
+            cout << "Tersisa " << 2 - counter << " percobaan lagi" << endl;
+            counter++;
+        }
+    }
+
+    return isLogin;
 }
 
-void menu()
+int menu(int pilihan)
 {
     cout << "============================" << endl;
     cout << "Selamat Datang di Aplikasi" << endl;
     cout << "============================" << endl
          << endl;
     cout << "Menu" << endl;
-    cout << "[1] Menghitung Luas Lingkaran" << endl;
-    cout << "[2] Menghitung Luas Segitiga" << endl;
-    cout << "[3] Menghitung Hasil Faktorial" << endl;
+    cout << "[1] Tampilkan list Mechanical Keyboard" << endl;
+    cout << "[2] Tambah Mechanical Keyboard" << endl;
+    cout << "[3] Edit nama Keyboard" << endl;
+    cout << "[4] Hapus Keyboard" << endl;
     cout << "[0] Keluar" << endl;
-}
 
-float luasLingkaran(float jariJari)
-{
-    return 3.14 * jariJari * jariJari;
-}
+    cout << "Pilihan: ";
+    cin >> pilihan;
 
-float luasSegitiga(float alas, float tinggi)
-{
-    return 0.5 * alas * tinggi;
-}
-
-int faktorial(int n)
-{
-    if (n == 0)
-    {
-        return 1;
-    }
-    else
-    {
-        return n * faktorial(n - 1);
-    }
+    return pilihan;
 }
